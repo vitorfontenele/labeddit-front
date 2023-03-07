cfb52a9f52d4005ed70cd2039ce3df9e2a5e38d4
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-03-07 20:05:25 -0300

    Update LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,8 +2,19 @@ import "./style.css";
 
 const LoginPage = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="container">
+            <div id="heading-group">
+                <img src="logo.svg" alt="LabEddit Logo" />
+                <h1 id="title-login" className="page-title">LabEddit</h1>
+                <h4 id="subtitle-login">O projeto de rede social da Labenu</h4>
+            </div>
+            <form className="form" action="" method="post">
+                <input className="form-input" type="email" placeholder="E-mail" />
+                <input className="form-input" type="password" placeholder="Senha" />
+                <button id="primary-button-login" className="button primary-button" type="submit">Continuar</button>
+            </form>
+            <div id="divider-login" className="divider"></div>
+            <button className="button secondary-button">Crie uma conta</button>
         </div>
     )
 }
