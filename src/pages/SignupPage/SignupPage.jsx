d14f61753ff275581b61547a9911eba4a771825f
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-03-08 18:26:52 -0300

    Update SignupPage

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,5 +1,5 @@
 import "./style.css";
-import { useEffect } from "react";
+import { useState , useEffect } from "react";
 import InputBox from "../../components/InputBox";
 import useForm from "../../hooks/useForm";
 import useActiveFields from "../../hooks/useActiveFields";
@@ -7,14 +7,17 @@ import useActiveFields from "../../hooks/useActiveFields";
 const SignupPage = () => {
     const [form, onChange] = useForm({username: "", email: "", password: ""});
     const [activeFields, onActivation] = useActiveFields({username: false, email: false, password: false});
+    const [checked, setChecked] = useState(false);
 
     useEffect(() => {
         onActivation(form);
     }, [form]);
 
+    const handleCheck = (e) => {setChecked(previousState => !previousState)};
+
     return (
         <div className="container">
-            <h1 className="page-title">{"Olá, boas-vindas ao LabEddit ;)"}</h1>
+            <h1 id="title-signup" className="page-title">{"Olá, boas-vindas ao LabEddit ;)"}</h1>
             <form className="form" action="" method="post">
                 <InputBox 
                     id={"username-signup"} 
@@ -43,6 +46,12 @@ const SignupPage = () => {
                     label={"Senha"}
                     isFieldActive={activeFields.password}
                 />
+                <span id="policy-warning">Ao continuar, você concorda com nosso <a className="anchor anchor-weak" href="#">Contrato de usuário</a> e nossa <a className="anchor anchor-weak" href="#">Política de Privacidade</a></span>
+                <div id="policy-checkbox-box">   
+                    <input type="checkbox" name="" id="policy-checkbox" checked={checked} onChange={handleCheck} />
+                    <label id="policy-checkbox-label" htmlFor="policy-checkbox">Eu concordo em receber emails sobre coisas legais no Labeddit</label>
+                </div>
+                <button type="submit" className="button primary-button" >Cadastrar</button>
             </form>
         </div>
         
