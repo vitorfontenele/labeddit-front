1c1e07c2a9c97b75061480e7b6c06074c93b764c
Vitor Fontenele <vitortfontenele@gmail.com>
2023-03-17 02:19:44 -0300

    Change BASE_URL

--- src/constants/urls.jsx
+++ src/constants/urls.jsx
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3003";
+export const BASE_URL = "https://labeddit-dcnn.onrender.com";
 export const TOKEN_NAME = "token-labeddit";
 export const USER_ID = "labeddit-logged-user-id";
